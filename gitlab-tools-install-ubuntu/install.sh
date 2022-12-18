cba8b28af124aa89cfda57d8320f98d163af9f11
netzoundx <netzoundx@outlook.com>
2022-12-18 13:44:03 +0700

    add gitlab runner installation

---
+++ gitlab-tools-install-ubuntu/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install aws cli tool
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
+sleep 2
+
+# Install aws cli tool
+sudo apt install -y docker.io
+sudo systemctl enable docker
+sudo groupadd docker
+newgrp docker
+sleep 2
+
+# Install Gitlab Runner
+curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+sleep 1
+sudo dpkg -i gitlab-runner_amd64.deb
+sleep 2
+sudo systemctl enable gitlab-runner
